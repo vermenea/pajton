645fefcbb09c96fc4543337dbcc6dc45649ab093
vermenea <vermenea@gmail.com>
2024-05-08 14:15:25 +0200

    finished 9th task

--- tydz5/zad5.py
+++ tydz5/zad5.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-#zad1
+# #zad1
 print("1----------------")
 macierz3x2  = np.array([[1,2,3],[4,5,6]])
 macierz2x3 = np.array([[1,4],[2,5],[3,6]])
 print(macierz3x2)
 print(macierz2x3)
 
-#zad2
+# #zad2
 print("2----------------")
 mnozenie = np.dot(macierz2x3, macierz3x2)
 print(mnozenie)
 
-#zad3
+# #zad3
 print("3----------------")
 transp_macierz1 = macierz3x2.transpose()
 transp_macierz2 = macierz2x3.transpose()
@@ -23,29 +23,29 @@ print(transp_macierz2)
 print(transp_macierz1_shape)
 print(transp_macierz2_shape)
 
-#zad4
+# #zad4
 print("4----------------")
 mnozenie_transp= np.dot(transp_macierz1, transp_macierz2)
 print(mnozenie_transp)
 
-#zad5
+# #zad5
 print("5----------------")
 i = 0
 for row in transp_macierz1:
     i+=1
     print(f"Rząd: {i}: {row}")
   
-#zad6
+# #zad6
 print("6----------------")
 for i, kolumna in enumerate(macierz2x3.transpose()):
     print(f"nr {i}: {kolumna}")
     
-#zad7
+# #zad7
 print("7----------------")
-# wynik = (macierz2x3 - macierz3x2)
-# print(wynik)
+wynik = (macierz2x3 - macierz3x2)
+print(wynik)
 
-#zad8
+# #zad8
 print("8----------------")
 try:
     print(macierz2x3 - macierz3x2)
@@ -58,3 +58,32 @@ finally:
        print(transp) 
        print(macierz2x3 - transp)
 
+#zad9
+print("9----------------")
+while True:
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        operator = input("Enter operator (+, -, *, /, sqrt): ")
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2  
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            result = num1 / num2
+        else:
+            print("Invalid operator")
+            continue
+        print("Result: ", result)
+        
+    except ValueError:
+        print("Invalid input! Please enter a valid number")
+    choice = input("Do you want to continue? (yes/no): ")
+    if choice.lower() != "yes":
+        print("bye bye")
+        break
+
+    
+           
